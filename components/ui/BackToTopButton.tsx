f93db331046667f88b7f3568b3b652d1ecd17263
Kouassi Wilfried <Kouassi.w@gmail.com>
2025-06-18 18:06:51 +0200

    fix: affichage du boutton tothetop uniquement sur les ecrans de plus de 600px pour éviter la redondance avec celui du mobile

--- components/ui/BackToTopButton.tsx
+++ components/ui/BackToTopButton.tsx
@@ -20,13 +20,14 @@ export function BackToTopButton() {
             onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
             aria-label="Retour en haut"
             className="
-        fixed bottom-8 left-1/2 transform -translate-x-1/2
-        z-50 p-3 rounded-full shadow-xl
-        bg-gradient-to-r from-blue-500 to-purple-500
-        hover:from-blue-600 hover:to-purple-600
-        ring-4 text-gray-300 border-gray-400 /30
-        transition-all duration-300
-      "
+                hidden sm:inline-flex
+                fixed bottom-8 left-1/2 transform -translate-x-1/2
+                z-50 p-3 rounded-full shadow-xl
+                bg-gradient-to-r from-blue-500 to-purple-500
+                hover:from-blue-600 hover:to-purple-600
+                ring-4 ring-white/30
+                transition-all duration-300
+            "
         >
             <ChevronUp className="w-6 h-6 text-white" />
         </button>
