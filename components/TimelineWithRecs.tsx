5b5bf6dd72bd089560eb4e222bfb35e00e68af81
Kouassi Wilfried <Kouassi.w@gmail.com>
2025-07-07 12:16:33 +0200

    fix(aboutSection): correction du lien de téléchargement

--- components/TimelineWithRecs.tsx
+++ components/TimelineWithRecs.tsx
@@ -60,8 +60,8 @@ export function TimelineWithRecs() {
           </div>
 
           <a
-            href="../public/Wilfried_KOUASSI_CV.pdf"
-            download
+            href="/Wilfried_KOUASSI_CV.pdf"
+            download="Wilfried_KOUASSI_CV.pdf"
             className="w-full inline-flex items-center justify-center bg-gradient-to-r from-blue-500 to-indigo-500 text-white font-semibold py-3 px-6 rounded-lg shadow-lg hover:from-blue-600 hover:to-indigo-600 transition text-lg mt-4"
           >
             <FaDownload className="mr-2" />
